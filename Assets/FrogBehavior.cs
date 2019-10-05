1c5c29b92ca59d79154c9bf7d30950fd29886b5c
Yam0 <tage.akerstrom@gmail.com>
2019-10-05 15:09:40 +0200

    Added Bolt visual scripting

---
+++ Assets/FrogBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogBehavior : MonoBehaviour
+{
+
+    public float jump;
+    public bool isGrounded = false;
+    public Transform GroundCheck1;
+    public LayerMask groundLayer;
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(GroundCheck1.position, 0.15f, groundLayer); // checks if you are within 0.15 position in the Y of the ground
+    }
+
+    void Start()
+    {
+        
+    }
+
+}
