9bf2dc7975df89ed99cff03996437eada5ab83ee
Anton Wiklund <ixuz07@gmail.com>
2019-10-06 00:53:18 +0200

    Spark spell does damage

--- Assets/Scripts/SparkSpell.cs
+++ Assets/Scripts/SparkSpell.cs
@@ -4,7 +4,23 @@ using UnityEngine;
 
 public class SparkSpell : Spell {
 
+    public int damage = 30;
+
     public override void Start() {
         base.Start();
+    }
+
+    public override void OnSpellEnd() {
+        base.OnSpellEnd();
+
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, 1f);
+        foreach (Collider2D collider in hitColliders) {
+            if (collider.gameObject.layer == LayerMask.NameToLayer("Enemy")) {
+                Enemy enemy = collider.gameObject.GetComponent<Enemy>();
+                if (enemy != null) {
+                    enemy.TakeDamage(damage);
+                }
+            }
+        }
     }
 }
