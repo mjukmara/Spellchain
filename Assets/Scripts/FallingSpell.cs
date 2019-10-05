f6dda3043afa445f4f18abd78a2de40f640ee6dd
Anton Wiklund <ixuz07@gmail.com>
2019-10-06 00:16:27 +0200

    Falling spell

---
+++ Assets/Scripts/FallingSpell.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSpell : Spell {
+
+    public override void Start() {
+        base.Start();
+    }
+
+    void Update() {
+
+    }
+
+    public override void OnNextSpell(GameObject nextSpell, int index) {
+        nextSpell.transform.rotation = Quaternion.Euler(0, 0, 90);
+    }
+}
