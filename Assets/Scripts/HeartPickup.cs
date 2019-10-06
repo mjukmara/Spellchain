cd5a565ca4f60ab8d2c5502f4d4743e9b6fc62f3
Anton Wiklund <ixuz07@gmail.com>
2019-10-06 22:30:46 +0200

    Added heart pickup

---
+++ Assets/Scripts/HeartPickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartPickup : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.tag == "Player") {
+            Player player = collider.gameObject.GetComponent<Player>();
+            player.healthBar.hp = player.healthBar.maxHp;
+            player.healthBar.UpdateGreenBar();
+            Destroy(gameObject);
+        }
+    }
+}
