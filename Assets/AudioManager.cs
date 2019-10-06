ab364af85d6cd0113257d66d0992d1f8aa52e76c
Anton Wiklund <ixuz07@gmail.com>
2019-10-06 21:05:52 +0200

    Added audio manager

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public Clip[] clips;
+
+    Dictionary<string, Clip> lookup = new Dictionary<string, Clip>();
+
+    void Start() {
+        foreach (Clip clip in clips) {
+            lookup.Add(clip.name, clip);
+        }
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            PlaySfx("Hit");
+        }
+    }
+
+    public void Play(string name) {
+        Clip clip;
+        lookup.TryGetValue(name, out clip);
+        if (clip != null) {
+            audioSource.PlayOneShot(clip.clip, clip.volume);
+        }
+    }
+
+    [System.Serializable]
+    public class Clip {
+        public string name;
+        public AudioClip clip;
+        public float volume = 1f;
+    }
+
+    public static void PlaySfx(string name) {
+        AudioManager audioManager = FindObjectOfType<AudioManager>();
+        audioManager.Play(name);
+    }
+}
