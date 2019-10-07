05a91a73bbea172ecb95f60faaa943e4e4647b78
Anton Wiklund <ixuz07@gmail.com>
2019-10-07 22:14:17 +0200

    Changes

---
+++ Assets/FadeScreen.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeScreen : MonoBehaviour {
+
+    public static FadeScreen instance;
+    public Animator animator;
+    public bool ignoreSplash = false;
+
+    void Awake() {
+
+        animator.SetBool("fadeIn", true);
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        animator.SetBool("ignoreSplash", ignoreSplash);
+    }
+
+    public void FadeIn() {
+        animator.SetBool("fadeIn", true);
+    }
+
+    public void FadeOut() {
+        animator.SetBool("fadeIn", false);
+    }
+
+    public void LoadScene(string sceneName) {
+        StartCoroutine(TransitionToScene(sceneName));
+    }
+
+    public void QuitApp() {
+
+        StartCoroutine(TransitionToQuit());
+    }
+
+    IEnumerator TransitionToScene(string sceneName) {
+        FadeOut();
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(sceneName);
+        yield return new WaitForSeconds(0.25f);
+        FadeIn();
+    }
+
+    IEnumerator TransitionToQuit() {
+        FadeOut();
+        yield return new WaitForSeconds(1);
+        Application.Quit();
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,11 +8,20 @@ public class Player : MonoBehaviour {
     public PlayerSpells playerSpells;
     public HealthBar healthBar;
     public HealthBar cooldownBar;
+    private bool loadOnce = false;
 
     void Update() {
         cooldownBar.maxHp = (int)(playerSpells.getMaxCooldown()*100);
         cooldownBar.hp = (int)(playerSpells.getCooldown()*100);
         cooldownBar.UpdateGreenBar();
+
+        if (healthBar.hp == 0) {
+            if (loadOnce == false) {
+                var FadeScreen = FindObjectsOfType<FadeScreen>();
+                FadeScreen[0].LoadScene("Map");
+                loadOnce = true;
+            }
+        }
     }
 
     public void TakeDamage(int damage) {
